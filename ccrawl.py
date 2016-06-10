61eab7dcc8405a1c56681a3f00b05c29cccfbcee
tastyminerals <tastyminerals@gmail.com>
2016-06-10 23:37:38 +0000

    added email search,  added older archives search, fixes

--- ccrawl.py
+++ ccrawl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Simple crawler for Corpora and BioNLP mailing lists.
+Simple crawler for Corpora mailing list.
 Searches the mailing list for keywords.
 """
 from __future__ import division
@@ -43,23 +43,29 @@ class do:
 MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
           'August', 'September', 'October', 'November', 'December']
 YEARS = [str(year) for year in range(1995, 2017)]
-JUNK = r'\[ date \]|\[ thread \]|\[ subject \]|\[ author \]|\[Corpora-List\]|\n\n\n+'
+JUNK = r'|'.join(['\[ date \]', '\[ thread \]', '\[ subject \]', '\[ author \]', 
+                  '\[Corpora-List\]', '\n\n\n+', 'Previous message:.*\n', 
+                  'Next message:.*\n', 'Messages sorted by:.*\n', 
+                  'More information about the Corpora-archive.*'])
 # http://mailman.uib.no//public/corpora/2016-June/thread.html
 URLS = ['http://mailman.uib.no//public/corpora/{0}-{1}/{2}',
-        'http://mail.bionlp.org/pipermail/bionlp_bionlp.org/{0}-{1}/{2}']
+        'http://clu.uni.no/corpora/{0}-{1}/{2}']
 SESSION = requests.session()
 
 
 def show_completed(complete):
     # Display a simple progress bar
-    sys.stdout.write('\rCompleted {0}%'.format(100 - complete))
+    done = 100 - complete
+    if done > 100:
+        done = 100
+    sys.stdout.write('\rCompleted {0}%'.format(done))
     sys.stdout.flush()
 
 
 def create_db(data):
     # Pickle retrived dict
     pickle.dump(data, open( ".corpora_list.pickle", "wb" ))
-    print "CORPORA-list has been stored!"
+    print "\nCORPORA-list has been stored!"
 
 
 def load_db():
@@ -80,11 +86,42 @@ def load_db():
     return loaded_corpora
 
 
-def _deep_sync(corpora, thread_urls, thread, year, month):
+def _include_older(corpora):
+    # Retieve data from older archives
+    # Start sync loop
+    print 'Retrieving CORPORA data from 1995-2004 (might take ~20 min!)...'
+    period = YEARS[:9]
+    diff, complete = int(round(100 / len(period))), 100
+    for i, year in enumerate(period):
+        show_completed(complete)
+        for i, _ in enumerate(MONTHS):
+            link = URLS[1].format(year, i, '')
+            # Get threads list
+            threads = SESSION.get(link)
+            threads_content = bs4.BeautifulSoup(threads.content, 'lxml')
+            if threads_content.title.text == '404 Not Found':
+                continue
+            # Get urls per thread
+            thread_hrefs = threads_content.find_all('a', href=True)
+            thread_urls = [turl['href'] for turl in thread_hrefs
+                           if turl['href'].endswith('.html')]
+            if not thread_urls:
+                continue
+            thread_text = re.sub(JUNK, '',
+                                 threads_content.get_text().encode('utf-8'))
+            thread = (link, thread_text)
+            corpora[thread] = {}
+            corpora = _deep_sync(corpora, thread_urls, thread, year, i, 1)
+        complete -= diff
+        show_completed(complete)
+    return corpora
+
+
+def _deep_sync(corpora, thread_urls, thread, year, month, n=0):
     # Sync deep (retrieve email data per subject)
     # Get data per subject
     for url in thread_urls:
-        email_url = MLIST.format(year, month, url)
+        email_url = URLS[n].format(year, month, url)
         email = SESSION.get(email_url)
         email_cont = bs4.BeautifulSoup(email.content, 'lxml')
         email_data = email_cont.get_text().encode('utf-8')
@@ -102,16 +139,18 @@ def sync(deep=False):
     last_month, last_year = up_cont.find_all('td')[3].text.strip(':').split()
     # Start sync loop
     corpora_data = {}
-    period = YEARS[9:]  # FIXIT
+    period = [str(year) for year in range(1995, int(last_year))][9:]
     diff, complete = int(round(100 / len(period))), 100
     for i, year in enumerate(period):
         # display % completed
         show_completed(complete)
         for month in MONTHS:
-            link = MLIST.format(year, month, suffix)
+            link = URLS[0].format(year, month, suffix)
             # Get threads list
             threads = SESSION.get(link)
             threads_content = bs4.BeautifulSoup(threads.content, 'lxml')
+            if threads_content.title.text == '404 Not Found':
+                continue
             # Get urls per thread
             thread_hrefs = threads_content.find_all('a', href=True)
             thread_urls = [turl['href'] for turl in thread_hrefs
@@ -127,12 +166,12 @@ def sync(deep=False):
                                           thread, year, month)
         complete -= diff
         show_completed(complete)
-    create_db(corpora_data)
     return corpora_data
 
 
-def search(corpora, query):
+def search(corpora):
     # Search CORPORA for keywords
+    query = args.find or args.dfind
     results = []
     for thread, subject in corpora.items():
         header = '>>> {0}{1}{2}'.format(do.BOLD, thread[0], do.END)
@@ -144,7 +183,7 @@ def search(corpora, query):
         for line in th_lines:
             # search for user query match
             found = re.search(query, line, flags=re.IGNORECASE)
-            if found:
+            if found and not args.dfind:
                 if first:
                     print '-' * len(header)
                     print header
@@ -152,39 +191,44 @@ def search(corpora, query):
                 thr_str = ''.join([do.BRED, r'\1', do.END])
                 thr_found = re.sub('({0})'.format(query), thr_str,
                                    found.string, flags=re.IGNORECASE)
-                print thr_found
-
+            if args.dfind and not args.find:
+                for subj_url, body in subject.items():
+                    if re.search(query, body, flags=re.IGNORECASE):
+                        print ">>> ", subj_url
+    
 
 def main():
+    if args.sync:
+        ans = raw_input("Perform deep sync (include emails, might take ~25 min!)? (y/n): ")
+        if ans:
+            corp = sync(True)
+        else:
+            corp = sync()
+        create_db(corp)
+        sys.exit()
+    # load local db
     corp = load_db()
-    search(corp, args.find)
+    if args.old:
+        corp = _include_older(corp)
+        create_db(corp)
+        sys.exit()
+    search(corp)
 
 
 if __name__ == '__main__':
     prs = argparse.ArgumentParser(description="""
-    Simple CORPORA and BioNLP mailing list crawler.""")
+    Simple CORPORA mailing list crawler.""")
     prs.add_argument('-f', '--find', type=str,
-                     help='specify a search keyword.',
-                     required=True)
+                     help='specify a search keyword (search thread titles).',
+                     required=False)
+    prs.add_argument('-df', '--dfind', type=str,
+                     help='specify a search keyword (search emails).',
+                     required=False)
     prs.add_argument('-s', '--sync', action='store_true',
                      help='syncronize with CORPORA-list.',
                      required=False)
     prs.add_argument('-old', '--old', action='store_true',
                      help='include older archives dating back to 1995.',
                      required=False)
-    prs.add_argument('-m', '--mlist', type=int,
-                     help='specify a mailing list: 1 -> CORPORA, 2 -> BIONLP,'+
-                     'default 1.', default=1,
-                     required=False)
     args = prs.parse_args()
-    MLIST = args.mlist
-    if args.mlist == 1:
-        MLIST = URLS[0]
-    elif args.mlist == 2:
-        MLIST = URLS[1]
-    else:
-        print 'ERROR! Incorrect mailing list.'
-        sys.exit(1)
     main()
-
-
